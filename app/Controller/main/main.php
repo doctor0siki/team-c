fdc381e92e527ea4ce385032c7edfb0e17aa8c91
Lily0339 <bebejuli7339@gmail.com>
2019-08-09 13:48:42 +0900

    fix room_select

--- app/Controller/main/main.php
+++ app/Controller/main/main.php
@@ -3,14 +3,17 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Model\Dao\ParentAttribute;
+use Model\Dao\RoomUser;
+use Model\Dao\Room;
 
 $app->get('/main/', function(Request $request, Response $response){
   $data = $request->getQueryParams();
 
   $parent_attribute = new ParentAttribute($this->db);
+  $room_user = new RoomUser($this->db);
 
   $param = array();
   $data["parent_attributes"] = $parent_attribute->select($param, "", "", "", true);
-  // var_dump($data);
+  $data["room_users"] = $room_user->get_room_user();
   return $this->view->render($response, '/main/main.twig', $data);
 });
